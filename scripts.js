90f3981518d319acd09b3475ef638d04dd0b8030
Fatima Rakiep <rakiepfatima@gmail.com>
2025-09-19 21:41:57 +0200

    Updated scripts.js
    Corrected spelling mistakes

--- scripts.js
+++ scripts.js
@@ -5,10 +5,10 @@ const initialTasks = [
   { id: 3, title: "Keep on Going 🏆", description: "You're almost there", status: "doing" },
   { id: 11, title: "Learn Data Structures and Algorithms 📚", description: "Study fundamental data structures and algorithms to solve coding problems efficiently", status: "todo" },
   { id: 12, title: "Contribute to Open Source Projects 🌐", description: "Gain practical experience and collaborate with others in the software development community", status: "done" },
-  { id: 13, title: "Build Portfolio Projects 💪", description: "Create a portfolio showcasing your skills and projects to potential employers", status: "done" }
+  { id: 13, title: "Build Portfolio Projects 🛠️", description: "Create a portfolio showcasing your skills and projects to potential employers", status: "done" }
 ];
 
-// DOM references for eaxh task category
+// DOM references for each task category
 const columns = {
   todo: document.querySelector('[data-status="todo"] .tasks-container'),
   doing: document.querySelector('[data-status="doing"] .tasks-container'),
